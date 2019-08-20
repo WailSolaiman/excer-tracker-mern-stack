4c3802b1a42cd5d1c1c9d4853f86ec9b6150cc4d
Wail Solaiman <waily2@hotmail.com>
2019-08-20 04:20:01 +0200

    Edited server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connection.once('open', () => {
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'client', 'build', 'index.html')))
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 const exercisesRouter = require('./routes/exercises')
 const userRouter = require('./routes/user')
