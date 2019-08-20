fc4968de09d5aaeea7942e7c30a4aee5522531a0
Wail Solaiman <waily2@hotmail.com>
2019-08-20 04:07:38 +0200

    Created deploy folder

---
+++ client/build/precache-manifest.108192197150bf1dc776f3ca8e4467b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de651c3629b96c4a9a18edac27e139d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b418fd1eba9f2e90ec4",
+    "url": "/static/css/2.a73d7da8.chunk.css"
+  },
+  {
+    "revision": "5b418fd1eba9f2e90ec4",
+    "url": "/static/js/2.40bff174.chunk.js"
+  },
+  {
+    "revision": "fe8abe40bf84e982b691",
+    "url": "/static/js/main.5d457ae6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
